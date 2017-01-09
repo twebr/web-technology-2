fb06d7cd668a506a3ef1eac894bc2e61ec70a062
Thijs Meeuwisse <thijs@twebber.nl>
2017-01-09 16:53:45 +0100

    Added migration for task deadline

---
+++ database/migrations/2017_01_09_154113_add_deadline_to_tasks.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDeadlineToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('deadline');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('deadline');
+        });
+    }
+}
